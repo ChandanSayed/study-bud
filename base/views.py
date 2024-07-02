e87e26295b46f3a3058f39d4f6a6f47447ee9a51
ChandanSayed <hangzhou635@gmail.com>
2024-07-02 21:38:23 +0600

    room details page

--- base/views.py
+++ base/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Room
 # Create your views here.
 
 
 def home(request):
-  return HttpResponse('Home')
+  rooms = Room.objects.all()
+  print(rooms)
+  context = {
+    'all_rooms': rooms,
+  }
+  template = loader.get_template('base/home.html')
+  return HttpResponse(template.render(context,request))
 
-def room(request):
+def room(request,pk):
+  room = Room.objects.get(id=pk)
+  print()
+  context = {
+    'room': room,
+  }
   template = loader.get_template('base/room.html')
-  return HttpResponse(template.render())
+  return HttpResponse(template.render(context,request))
