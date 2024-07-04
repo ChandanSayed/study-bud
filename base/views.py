fec4ebc42699613b69a96fe935c32366f4c49a20
ChandanSayed <hangzhou635@gmail.com>
2024-07-05 03:24:20 +0600

    Search

--- base/views.py
+++ base/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
-from .models import Room
+from django.db.models import Q 
+from .models import Room,Topic
 from .forms import RoomForm
 # Create your views here.
 
 
 def home(request):
-  rooms = Room.objects.all().order_by('-updated','-created')
-  print(rooms)
+  q = request.GET.get('q') if request.GET.get('q') != None else ''
+  rooms = Room.objects.filter(
+    Q(topic__name__icontains = q)
+    | Q(name__icontains = q )|
+    Q(description__icontains = q )
+    ).order_by('-updated','-created')
+  room_count = rooms.count()
+  topics = Topic.objects.all()
   context = {
     'all_rooms': rooms,
+    'all_topics': topics,
+    'room_count': room_count
   }
   template = loader.get_template('base/home.html')
   return HttpResponse(template.render(context,request))
